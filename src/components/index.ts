3bde06b148c9d32d80f4ba40d695921680b5e30f
stanislav_m <allrightsm@yandex.ru>
2025-11-20 00:40:53 +0300

    Немного изменил рендеринг новых сообщений

--- src/components/index.ts
+++ src/components/index.ts
@@ -17,3 +17,4 @@ export { StreamingMessage } from './StreamingMessage';
 export { ModelSelector } from './ModelSelector';
 export { DesktopLayout } from './layouts/DesktopLayout';
 export { MobileLayout } from './layouts/MobileLayout';
+export { TypingIndicator } from './TypingIndicator'; 
